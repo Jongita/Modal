4f75a9865ca219b4ef3df0fa782cb64fba087272
Jongita <jurgitajonaityte@gmail.com>
2024-05-16 15:52:14 +0300

    final

---
+++ script.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector('button');
+const modal = document.querySelector('.modal');
+const modalContent = document.querySelector('.modalContent');
+const close = document.querySelector('.close');
+
+btn.addEventListener('click', openModal);
+close.addEventListener("click", closeModal)
+modal.addEventListener("click", closeModal)
+
+function openModal() {
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    modalContent.classList.add("slide-up")
+
+    setTimeout(() => {
+        modalContent.classList.remove("slide-up")
+        modal.style.display = "none"
+    }, 500)
+}
